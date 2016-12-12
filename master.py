2dff98b7c6e3737c51caeedbb51a2f31933018e6
Fredrik Löwenhamn <lowet84@gmail.com>
2016-12-12 13:40:36 +0100

    Code stuff

--- master.py
+++ master.py
@@ -1,12 +1,12 @@
+import codecs
 import json
 import urllib.request
 
 
 def setup_master():
     nodes_json = urllib.request.urlopen("http://localhost:8080/api/v1/nodes").read()
-
-    print(nodes_json)
-    nodes = json.loads(nodes_json)
+    reader = codecs.getreader("utf-8")
+    nodes = json.loads(reader(nodes_json))
 
     print(nodes)
 
