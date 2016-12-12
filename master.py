8246df157f8a1874d199755298f9d2ead59e8c51
Fredrik Löwenhamn <lowet84@gmail.com>
2016-12-12 13:26:48 +0100

    Code stuff

--- master.py
+++ master.py
@@ -2,7 +2,7 @@ import urllib.request
 
 
 def setup_master():
-    nodes = urllib.request.urlopen("http://localhost:8080/api/vi/nodes").read()
+    nodes = urllib.request.urlopen("http://localhost:8080/api/v1/nodes").read()
 
     print(nodes)
 
