aeaccaca2ba4d7cc7b1c61e879ecdbaef8f5461f
Fredrik Löwenhamn <lowet84@gmail.com>
2016-12-12 14:05:53 +0100

    Code stuff

--- master.py
+++ master.py
@@ -2,11 +2,24 @@ import codecs
 import json
 import urllib.request
 
-
 def setup_master():
     nodes_json = urllib.request.urlopen("http://localhost:8080/api/v1/nodes")
     reader = codecs.getreader("utf-8")
-    nodes = json.load(reader(nodes_json))
+    data = json.load(reader(nodes_json))
 
-    print(nodes)
+    # with open('nodes.txt') as data_file:
+    #     data = json.load(data_file)
 
+    storages = []
+
+    for node in data['items']:
+        hostname = node['metadata']['labels']['kubernetes.io/hostname']
+        if 'role' in node['metadata']['labels']:
+            role = node['metadata']['labels']['role']
+        else:
+            role = None
+
+        if role == 'storage':
+            storages.append(hostname)
+
+    print("Fin!")
